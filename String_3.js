2c5a5245bebd6237c55e2d5695b640f4a8a1a771
Gopinath Rajamanickam <vrmgopi@gmail.com>
2022-08-27 12:10:13 +0530

    Modified the local changes

--- String_3.js
+++ String_3.js
@@ -3,7 +3,7 @@ const sArray = s.split("");
 const fin = new Set(sArray);
 console.log(fin.size);
 if(fin.size===3){
-    console.log("Wonder");
+    console.log("Wonder        ");
 }
 else{
     console.log(-1);
